4b3fa44414b853a186ff3b75b6dd8f6fbeffbfaa
Vadim Ziatdinov <optimistical@bk.ru>
2026-05-05 22:01:27 +0500

    fixed the bracket break

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -11,8 +11,7 @@ type Personal struct {
 
 func (p Personal) Print() {
     // TODO: реализовать функцию
-    fmt.Printf("Имя: %s\n", p.Name
-)
+    fmt.Printf("Имя: %s\n", p.Name)
     fmt.Printf("Вес: %.2f\n", p.Weight)
     fmt.Printf("Рост: %.2f\n", p.Height)
 }
